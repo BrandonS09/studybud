5fefb102b804d40cb05704d8df2d7440f2471ba6
Brandon <brandonshen123@gmail.com>
2024-06-10 15:03:21 -0700

    finished adding theme and also created an edit user page

--- base/views.py
+++ base/views.py
@@ -147,3 +147,15 @@ def deleteMessage(request,pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form':form})
+
